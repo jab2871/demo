18775d42e4c589a9e8d3065a00e2daca9c38a4e2
Jonathan Hodgson <git@jonathanh.co.uk>
2020-08-07 21:08:49 +0100

    Adds chicken

--- greeting.py
+++ greeting.py
@@ -11,6 +11,9 @@ def cat():
 def cow():
     print("moo")
 
+def chicken():
+    print("cluck")
+
 def main():
     """Main Function
     :returns: TODO
@@ -22,6 +25,8 @@ def main():
         cat()
     elif len(sys.argv) > 1 and sys.argv[1] == "cow":
         cow()
+    elif len(sys.argv) > 1 and sys.argv[1] == "chicken":
+        chicken()
     else:
         print("HELLO WORLD")
 
