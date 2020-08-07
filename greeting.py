443cd396aff32ba1638f2b7229481ecd0ff1a900
Jonathan Hodgson <git@jonathanh.co.uk>
2020-08-07 20:27:46 +0100

    Makes a dog say woof

--- greeting.py
+++ greeting.py
@@ -2,12 +2,18 @@
 
 import sys
 
+def dog():
+    print("woof")
+
 def main():
     """Main Function
     :returns: TODO
 
     """
-    print("HELLO WORLD")
+    if len(sys.argv) > 1 and sys.argv[1] == "dog":
+        dog()
+    else:
+        print("HELLO WORLD")
 
 if __name__ == "__main__":
     main()
